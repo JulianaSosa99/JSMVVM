a681db4d978dd0db801a3f8c2811b770d8e56bf9
Kamila <juliana.sosa@udla.edu.ec>
2024-06-16 00:42:45 -0500

    Ingreso de propiedades sobre la infromación de la app, como nombre y version

--- JSNotesMaui/ViewModels/JSAboutViewModel.cs
+++ JSNotesMaui/ViewModels/JSAboutViewModel.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace JSNotesMaui.ViewModels
 {
-    class JSAboutViewModel
+    internal class JSAboutViewModel
     {
+        public string JSTitle => AppInfo.Name;
+        public string JSVersion => AppInfo.VersionString;
+        public string JSMoreInfoUrl => "https://aka.ms/maui";
+        public string JSMessage => "This app is written in XAML and C# with .NET MAUI. POR JULIANA SOSA";
+        public ICommand ShowMoreInfoCommand { get; }
+
+        public JSAboutViewModel()
+        {
+            ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+        }
+
+        async Task ShowMoreInfo() =>
+            await Launcher.Default.OpenAsync(JSMoreInfoUrl);
     }
 }
