b20d58bd33780c1b203cbfcefa9c143fbbed2221
Kamila <juliana.sosa@udla.edu.ec>
2024-06-16 00:47:32 -0500

    Elimine codigo ya que ya no se estaba usando y dejamos solo el constructor

--- JSNotesMaui/Views/JSAboutPage.xaml.cs
+++ JSNotesMaui/Views/JSAboutPage.xaml.cs
@@ -7,17 +7,6 @@ public partial class JSAboutPage : ContentPage
 		InitializeComponent();
 	}
 
-    private async void LearnMore_Clicked(object sender, EventArgs e)
-    {
-        //creacion del boton LeanMoreClicked
-        if (BindingContext is Models.JSAbout about)
-        {
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-
-
-        }
-
-
-    }
+    
 
 }
