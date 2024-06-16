c5fc2983c720a4158460c20b15567ec3569c2362
Kamila <juliana.sosa@udla.edu.ec>
2024-06-16 11:24:18 -0500

    Cambios dentro de los View Models, tambien resolucion de problemas ya que se cambiaron nombres de varias variables

--- JSNotesMaui/ViewModels/JSNotesViewModel.cs
+++ JSNotesMaui/ViewModels/JSNotesViewModel.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using JSNotesMaui.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace JSNotesMaui.ViewModels
 {
-    class JSNotesViewModel
+   
+    internal class JSNotesViewModel: IQueryAttributable
     {
+        public JSNotesViewModel()
+        {
+            JSAllNotes = new ObservableCollection<ViewModels.JSNoteViewModel>(Models.JSNote.LoadAll().Select(n => new JSNoteViewModel(n)));
+            NewCommand = new AsyncRelayCommand(NewNoteAsync);
+            SelectNoteCommand = new AsyncRelayCommand<ViewModels.JSNoteViewModel>(SelectNoteAsync);
+        }
+
+        public ObservableCollection<ViewModels.JSNoteViewModel> JSAllNotes { get; }
+        public ICommand NewCommand { get; }
+        public ICommand SelectNoteCommand { get; }
+
+        private async Task NewNoteAsync()
+        {
+            await Shell.Current.GoToAsync(nameof(Views.JSNotePage));
+        }
+
+        private async Task SelectNoteAsync(ViewModels.JSNoteViewModel note)
+        {
+            if (note != null)
+                await Shell.Current.GoToAsync($"{nameof(Views.JSNotePage)}?load={note.JSIdentifier}");
+        }
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+{
+        if (query.ContainsKey("deleted"))
+        {
+            string noteId = query["deleted"].ToString();
+            JSNoteViewModel matchedNote = JSAllNotes.Where((n) => n.JSIdentifier == noteId).FirstOrDefault();
+
+        // If note exists, delete it
+        if (matchedNote != null)
+            JSAllNotes.Remove(matchedNote);
     }
+    else if (query.ContainsKey("saved"))
+    {
+        string noteId = query["saved"].ToString();
+        JSNoteViewModel matchedNote = JSAllNotes.Where((n) => n.JSIdentifier == noteId).FirstOrDefault();
+
+        // If note is found, update it
+        if (matchedNote != null)
+                {
+                    matchedNote.JSReload();
+                    JSAllNotes.Move(JSAllNotes.IndexOf(matchedNote), 0);
+                }
+            
+
+        // If note isn't found, it's new; add it.
+        else
+                    JSAllNotes.Insert(0, new JSNoteViewModel(Models.JSNote.Load(noteId)));
+            }
+}
+
+    }
+  
 }
--- JSNotesMaui/Views/JSAllNotesPage.xaml.cs
+++ JSNotesMaui/Views/JSAllNotesPage.xaml.cs
@@ -5,35 +5,13 @@ public partial class JSAllNotesPage : ContentPage
     public JSAllNotesPage()
     {
         InitializeComponent();
-        BindingContext = new Models.JSAllNotes();
+       
 
     }
-    protected override void OnAppearing()
+
+    private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
     {
-        ((Models.JSAllNotes)BindingContext).LoadNotes();
+        notesCollection.SelectedItem = null;
     }
-
-    private async void Add_Clicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync(nameof(JSNotePage));
-    }
-
-    private async void notesCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        if (e.CurrentSelection.Count != 0)
-        {
-            // Get the note model
-            var note = (Models.JSNote)e.CurrentSelection[0];
-
-            // Should navigate to "NotePage?ItemId=path\on\device\XYZ.notes.txt"
-
-            await Shell.Current.GoToAsync($"{nameof(JSNotePage)}?{nameof(JSNotePage.ItemId)}={note.Filename}");
-
-            // Unselect the UI
-            notesCollection.SelectedItem = null;
-        }
-    }
-
-
 
 }
