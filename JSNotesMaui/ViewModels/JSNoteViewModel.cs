643408923381072c2f7429b80dff39823cec471b
Kamila <juliana.sosa@udla.edu.ec>
2024-06-16 01:16:27 -0500

    Cambios dentro de NoteViewModel además cambie partes de la interfaz de XAML ya que no se usas

--- JSNotesMaui/ViewModels/JSNoteViewModel.cs
+++ JSNotesMaui/ViewModels/JSNoteViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,73 @@ using System.Threading.Tasks;
 
 namespace JSNotesMaui.ViewModels
 {
-    class JSNoteViewModel
+    internal class JSNoteViewModel : ObservableObject, IQueryAttributable
+
     {
+        public JSNoteViewModel()
+        {
+            _jsnote = new Models.JSNote();
+            JSSaveCommand = new AsyncRelayCommand(JSSave);
+            JSDeleteCommand = new AsyncRelayCommand(JSDelete);
+        }
+        public JSNoteViewModel(Models.JSNote note)
+        {
+            _jsnote = note;
+            JSSaveCommand = new AsyncRelayCommand(JSSave);
+            JSDeleteCommand = new AsyncRelayCommand(JSDelete);
+        }
+        private async Task JSSave()
+        {
+            _jsnote.Date = DateTime.Now;
+            _jsnote.JSSave();
+            await Shell.Current.GoToAsync($"..?saved={_jsnote.Filename}");
+        }
+
+        private async Task JSDelete()
+        {
+            _jsnote.JSDelete();
+            await Shell.Current.GoToAsync($"..?deleted={_jsnote.Filename}");
+        }
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("load"))
+            {
+                _jsnote = Models.JSNote.Load(query["load"].ToString());
+                JSRefreshProperties();
+            }
+        }
+        public void JSReload()
+        {
+            _jsnote = Models.JSNote.Load(_jsnote.Filename);
+            JSRefreshProperties();
+        }
+
+        private void JSRefreshProperties()
+        {
+            OnPropertyChanged(nameof(JSText));
+            OnPropertyChanged(nameof(JSDate));
+        }
+
+        private Models.JSNote _jsnote;
+
+        public string JSText
+        {
+            get => _jsnote.Text;
+            set
+            {
+                if (_jsnote.Text != value)
+                {
+                    _jsnote.Text = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public ICommand JSSaveCommand { get; private set; }
+        public ICommand JSDeleteCommand { get; private set; }
+
+        public DateTime JSDate => _jsnote.Date;
+
+        public string JSIdentifier => _jsnote.Filename;
     }
+
 }
--- JSNotesMaui/Views/JSNotePage.xaml.cs
+++ JSNotesMaui/Views/JSNotePage.xaml.cs
@@ -1,60 +1,14 @@
 namespace JSNotesMaui.Views;
-[QueryProperty(nameof(ItemId), nameof(ItemId))]
-
+ 
 public partial class JSNotePage : ContentPage
 
 {
-    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
-
+ 
     public JSNotePage()
     {
         InitializeComponent();
 
-        string appDataPath = FileSystem.AppDataDirectory;
-        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
-
-        LoadNote(Path.Combine(appDataPath, randomFileName));
+         
     }
 
-    private async void SaveButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.JSNote note)
-            File.WriteAllText(note.Filename, TextEditor.Text);
-
-        await Shell.Current.GoToAsync("..");
-
     }
-
-    private async void DeleteButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.JSNote note)
-        {
-
-
-            if (File.Exists(note.Filename))
-                File.Delete(note.Filename);
-        }
-        await Shell.Current.GoToAsync("..");
-     
-    }
-
-
-    private void LoadNote(string fileName)
-    {
-        Models.JSNote noteModel= new Models.JSNote();
-        noteModel.Filename= fileName;   
-
-      
-        if (File.Exists(fileName))
-        {
-            noteModel.Date = File.GetCreationTime(fileName);
-            noteModel.Text = File.ReadAllText(fileName);
-        }
-
-        BindingContext = noteModel;
-    }
-    public string ItemId
-    {
-        set { LoadNote(value); }
-    }
-}
