41d62c193c9215cd52fc1f8272f75f3cab0fff50
Kamila <juliana.sosa@udla.edu.ec>
2024-06-16 00:12:30 -0500

    Cambios en la clase JSNote dentro de la carpeta de Models

--- JSNotesMaui/Models/JSNote.cs
+++ JSNotesMaui/Models/JSNote.cs
@@ -3,14 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+ 
 
 namespace JSNotesMaui.Models
+
 {
+            
+
     internal class JSNote
     {
+
         public string Filename {  get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
 
+        public JSNote()
+        {
+            Filename = $"{Path.GetRandomFileName()}.notes.txt";
+            Date = DateTime.Now;
+            Text = "";
+        }
+
+        public void JSSave() =>
+            File.WriteAllText(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename), Text);
+
+        public void JSDelete() =>
+            File.Delete(System.IO.Path.Combine(FileSystem.AppDataDirectory, Filename));
+
+        public static JSNote Load(string filename)
+        {
+            filename = System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
+
+            if (!File.Exists(filename))
+                throw new FileNotFoundException("Juliana Sosa Unable to find file on local storage.", filename);
+
+            return
+                new()
+                {
+                    Filename = Path.GetFileName(filename),
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetLastWriteTime(filename)
+                };
+        }
+
+        public static IEnumerable<JSNote> LoadAll()
+        {
+            // Get the folder where the notes are stored.
+            string appDataPath = FileSystem.AppDataDirectory;
+
+            // Use Linq extensions to load the *.notes.txt files.
+            return Directory
+
+                    // Select the file names from the directory
+                    .EnumerateFiles(appDataPath, "*.notes.txt")
+
+                    // Each file name is used to load a note
+                    .Select(filename => JSNote.Load(Path.GetFileName(filename)))
+
+                    // With the final collection of notes, order them by date
+                    .OrderByDescending(note => note.Date);
+        }
+
+
     }
+
 }
